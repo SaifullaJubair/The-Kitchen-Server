4834f32e7e0a6daa81921d389557848a1b36eae6
SaifullaJubair <saifullajubair6@gmail.com>
2022-11-10 05:18:02 +0600

    .env use kore mongodb setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -9,6 +10,22 @@ app.use(express.json())
 app.get('/', (req, res) => {
    res.send('The Kitchen server is running')
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5fubjmc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+   try {
+      const serviceCollection = client.db('kitchen-a11').collection('services')
+   }
+   finally { }
+}
+run().catch(error => console.error(error));
+
+
+
 
 app.listen(port, () => {
    console.log(`genius car server is running on ${port}`);
