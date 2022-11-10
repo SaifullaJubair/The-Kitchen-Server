bee8a7804a683d27ba843cdf488bb0f1bc8b0f19
SaifullaJubair <saifullajubair6@gmail.com>
2022-11-10 19:08:39 +0600

    review get

--- index.js
+++ index.js
@@ -44,7 +44,15 @@ async function run() {
       })
 
       // reviews api 
-      app.post('reviews', async (req, res) => {
+      app.get('/reviews', async (req, res) => {
+         const query = {}
+         const cursor = reviewCollection.find(query)
+         const reviews = await cursor.toArray()
+         res.send(reviews)
+      })
+
+
+      app.post('/reviews', async (req, res) => {
          const review = req.body;
          const result = await reviewCollection.insertOne(review)
          res.send(result);
