d3a278dfb1d7a3c52f8c5da8d1573bacbee2fc10
SaifullaJubair <saifullajubair6@gmail.com>
2022-11-10 14:03:22 +0600

    services data show

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -14,11 +15,18 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5fubjmc.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = "mongodb+srv://kitchen-a11:1XiWjVFoMeGDWkIt@cluster0.5fubjmc.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
    try {
       const serviceCollection = client.db('kitchen-a11').collection('services')
+      app.get('/services', async (req, res) => {
+         const query = {};
+         const cursor = serviceCollection.find(query);
+         const services = await cursor.toArray();
+         res.send(services);
+      })
    }
    finally { }
 }
